60902b63e583732e2b62220eca8e5f1551600652
Margaret Lazarovits <mlazarovits@gmail.com>
2024-06-25 14:01:53 +0200

    updated to use pandas, introduced cleaning function for dataframe

--- ProcessData.py
+++ ProcessData.py
@@ -1,25 +1,24 @@
 import numpy as np
+import pandas as pd
 
 class CSVReader:
 	def __init__(self, file):
 		#data is a list of feature, label pairs or tuples
 		self._data = np.array([])
 		self._file = file
+		self._header = np.array([])
 
 	def ExtractData(self):
-		f = open(self._file)
-		header = f.readline().split(", ")
-		print(header)
-		basedata = np.genfromtxt(self._file, delimiter=",",skip_header=1)
-		dataset = [list(tup) for tup in basedata]
-		#separate labels from features here and store as ([features], label) in self._data
-		dataset = np.array(dataset)	
-		print(dataset.shape)
-		print(dataset[0])	
+		self._data = pd.read_csv(self._file)
 		
 	#data cleaning, cuts, etc.
-	#def CleanData(self):
-			
-					
-reader = CSVReader("csv/GJets_R17_v16_GJets_HT-400To600_AODSIM_RunIIFall17DRPremix_photons_defaultv3p2.csv")
+	def CleanData(self):
+		print("Cleaning data")
+		#remove any "unmatched" labels
+		self._data = self._data[self._data['label'] != -1]
+		#put extra cuts on subcluster energy, etc.	
+				
+#reader = CSVReader("csv/GJets_R17_v16_GJets_HT-400To600_AODSIM_RunIIFall17DRPremix_photons_defaultv3p2.csv")
+reader = CSVReader("csv/photonSkim_test_emAlpha0p500_thresh1p000_NperGeV0p100_GJets_HT-400To600_AODSIM_RunIIFall17DRPremix_output257_v16.csv")
 reader.ExtractData()
+reader.CleanData()
