e0c0c718b2c474517eb970e98a78cec9ef0a5c15
Margaret Lazarovits <mlazarovits@gmail.com>
2024-08-07 14:49:17 -0500

    finished workflow - added test + ROC plot methods

--- DeepNN.py
+++ DeepNN.py
@@ -1,25 +1,40 @@
 from ModelBase import ModelBase
-from tensorflow.python.keras import layers, metrics, Input, Model, activations
+from keras import layers, metrics, Input, Model, activations
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import RocCurveDisplay
+import os
 
 class DeepNeuralNetwork(ModelBase):
 	def __init__(self):
 		self._inputShape = None
 		self._nNodes = None
+		self._x = None
+		self._y = None
 		super().__init__()
 
-	def __init__(self, inputShape, nNodes):
-		#shape of input data
-		self._inputShape = inputShape
+	def __init__(self, data, nNodes):
 		#a list of ints that defines the nodes for each dense layer (obviously len(nNodes) == # layers
 		self._nNodes = nNodes
+		#extract inputs and labels, remove unnecessary columns
+		#drop event + subcl cols
+		dropcols = ["Event","subcl","label"]
+		#TODO:binary labels for now, will have to one-hot encode for multiclass
+		y = data["label"].to_numpy()
+		x = data.drop(dropcols,axis=1).to_numpy()
+		#80/20 train/test split
+		rand = 43 #change to random number to randomize
+		self._xtrain, self._xtest, self._ytrain, self._ytrue = train_test_split(x,y,test_size=0.2,random_state=rand)
+		print(self._xtrain.shape[0],"training samples")
+		#shape of input data
 		super().__init__()
 
 	def summary(self):
 		self._model.summary()
 
 	#fully connected network
-	def BuildModel(self):
-		input_layer = Input(shape=(1,self._inputShape))
+	def BuildModel(self,name = "model"):
+		input_layer = Input(shape=(self._xtrain.shape[1],))
 		
 		#reLu activation at internal layers
 		dense_layers = [layers.Dense(n,name="dense_layer"+str(i),activation=activations.relu) for i, n in enumerate(self._nNodes)]
@@ -31,7 +46,7 @@ class DeepNeuralNetwork(ModelBase):
 		output_layer = layers.Dense(1,activation=activations.sigmoid)
 		
 		x = output_layer(x) 
-		self._model = Model(inputs = input_layer, outputs = x)
+		self._model = Model(inputs = input_layer, outputs = x, name = name)
 
 	#SGD optimizer
 	#cross-entropy loss (binary or categorical depending on labeling scheme)
@@ -44,15 +59,50 @@ class DeepNeuralNetwork(ModelBase):
 		)
 
 
-	def VizLoss(self, history, fname):
+	def VizMetric(self, history, fname):
 		plt.figure()
-		plt.plot(history.history['val_loss'], label="val loss")
-		plt.plot(history.history['loss'],label="train loss")
-		plt.title("Loss")
+		plt.plot(history.history['val_'+fname], label="val "+fname)
+		plt.plot(history.history[fname],label="train "+fname)
+		plt.title(fname)
 		plt.legend()
-		plt.savefig("loss/"+fname)
+		#check if output dir exists
+		path = "results/"+self._model.name
+		if not os.path.exists(path):
+			os.mkdir(path)
+		plt.savefig(path+"/"+fname)
 
-	def TrainModel(self,x,y,viz:bool = False,verb= 0):
-		his = self._model.fit(x,y,epochs=1,verbose=verb)
+
+	#Caltech delayed photon analysis just plots fpr vs tpr for their DNN performance
+	#TODO:labels are on binary for now (0, 1) but will eventually be (0, 1, 2) for (sig, spike, BH)
+	#see https://scikit-learn.org/stable/auto_examples/model_selection/plot_roc.html#roc-curve-showing-a-specific-class
+	#for multiclass ROC (one-vs-rest = sig-vs-rest)
+	def VizROC(self, ytrue, ypred):
+		display = RocCurveDisplay.from_predictions(ytrue,ypred,
+			name="signal vs rest",
+			color="pink",
+			plot_chance_level=True,
+		)
+		display.ax_.set(
+			xlabel="False Positive Rate",
+			ylabel="True Positive Rate",
+			title="Signal vs (spike & beam halo) subcluster ROC"
+		)
+		#check if output dir exists
+		path = "results/"+self._model.name
+		if not os.path.exists(path):
+			os.mkdir(path)
+		plt.savefig(path+"/ROCplot")
+
+	def TrainModel(self,epochs=1,viz=False,verb=1):
+		#80/20 train/val split (of training data)
+		his = self._model.fit(self._xtrain,self._ytrain,epochs=epochs,verbose=verb,validation_split=0.2)
 		if viz:
-			self.VizLoss(his,"test")
+			self.VizMetric(his,"loss")
+			#can also add accuracy, etc.
+
+	def TestModel(self,batch_size=1,viz=False,verb=1):
+		ypred = self._model.predict(self._xtest,batch_size=batch_size,verbose=verb)
+		print("ytrue",self._ytrue,"ypred",ypred)
+		if viz:
+			self.VizROC(self._ytrue, ypred[0])
+				
--- runDNN.py
+++ runDNN.py
@@ -6,18 +6,18 @@ from DeepNN import DeepNeuralNetwork
 reader = CSVReader("csv/photonSkim_test_emAlpha0p500_thresh1p000_NperGeV0p100_GJets_HT-400To600_AODSIM_RunIIFall17DRPremix_output257_v16.csv")
 reader.CleanData()
 data = reader.GetData()
+#GJets csv has all unmatched labels
 
-print(data,data.shape)
 #len(nodes) = # layers
 #nodes[i] = # nodes at ith layer
-nodes = [64, 64, 64]
-datashape = data.shape
+nodes = [64, 64, 64] #simple-DNN
 
-model = DeepNeuralNetwork(datashape[1],nodes)
-model.BuildModel()
+model = DeepNeuralNetwork(data,nodes)
+model.BuildModel("simple-DNN")
 model.CompileModel()
 model.summary()
-#model.SetData(data) - cleans out unnecessary columsn (ie subcl #) and sets train/test split
-#model.TrainModel()
+#input is TrainModel(epochs=1,oname="",int:verb=1)
+#caltech: epochs = 50, batch size = 10k, early stopping, adaptive learning rate
+model.TrainModel(10,True)
 #needs test data + to make ROC plots
-#model.Evaluate(test_data, batch)
+model.TestModel(1,True)
