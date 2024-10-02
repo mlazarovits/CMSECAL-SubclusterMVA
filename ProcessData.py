469dd580da6768cf697cee7b1aa020fbad2d746b
Margaret Lazarovits <mlazarovits@gmail.com>
2024-10-02 12:19:50 -0500

    reindexing df when concatenating to avoid overlap in rows when dropping, dropping rows with nans too

--- ProcessData.py
+++ ProcessData.py
@@ -12,18 +12,38 @@ class CSVReader:
 
 	def AddFile(self,file):
 		data = pd.read_csv(file)
-		self._data = pd.concat([self._data,data])
-
+		self._data = pd.concat([self._data,data],ignore_index=True)
+	
 		
 	#data cleaning, cuts, etc.
 	def CleanData(self):
-		print("Cleaning data")
+		print("Cleaning data",len(self._data))
 		#remove any "unmatched" labels
 		self._data = self._data[self._data['label'] != -1]
+		print("after -1 removal",len(self._data))
+		sig = len(self._data[self._data["label"] == 0])
+		nom = len(self._data[self._data["label"] == 1])
+		gmsb = len(self._data[self._data["sample"].str.contains("GMSB") == True])
+		gjets = len(self._data[self._data["sample"].str.contains("GJets") == True])
+		print("sig",sig,"nom",nom,"gmsb",gmsb,"gjets",gjets)
 		#remove not-signal-matched photons in GMSB sample (this is not the "bkg" we want to target)
-		rowbool = (self._data["sample"].str.contains("GMSB")) & (self._data["label"] == 1) 
+		rowbool = ((self._data["sample"].str.contains("GMSB") == True) & (self._data["label"] == 1)) 
 		self._data = self._data.drop(self._data[rowbool].index)	 
+		print("after GMSB bkg removal",len(self._data))
+		sig = len(self._data[self._data["label"] == 0])
+		nom = len(self._data[self._data["label"] == 1])
+		gmsb = len(self._data[self._data["sample"].str.contains("GMSB") == True])
+		gjets = len(self._data[self._data["sample"].str.contains("GJets") == True])
+		print("sig",sig,"nom",nom,"gmsb",gmsb,"gjets",gjets)
 		#put extra cuts on subcluster energy, etc.	
+		#remove nans - TODO: see why this is happening with minor_length
+		self._data.dropna()
+		print('after dropna')
+		sig = len(self._data[self._data["label"] == 0])
+		nom = len(self._data[self._data["label"] == 1])
+		gmsb = len(self._data[self._data["sample"].str.contains("GMSB") == True])
+		gjets = len(self._data[self._data["sample"].str.contains("GJets") == True])
+		print("sig",sig,"nom",nom,"gmsb",gmsb,"gjets",gjets)
 				
 	def GetData(self):
 		return self._data
