7772ceccdbf8d44bc8912a64a59f913ed8c61296
Margaret Lazarovits <mlazarovits@gmail.com>
2024-10-07 10:14:41 -0500

    added printouts for data processing cutflow

--- ProcessData.py
+++ ProcessData.py
@@ -17,24 +17,32 @@ class CSVReader:
 		
 	#data cleaning, cuts, etc.
 	def CleanData(self):
-		print("Cleaning data",len(self._data))
+		print("Cleaning data",len(self._data),"subclusters initially")
 		#remove any "unmatched" labels
+		print("after unmatched removal")
 		self._data = self._data[self._data['label'] != -1]
-		print("after -1 removal",len(self._data))
 		sig = len(self._data[self._data["label"] == 0])
 		nom = len(self._data[self._data["label"] == 1])
 		gmsb = len(self._data[self._data["sample"].str.contains("GMSB") == True])
 		gjets = len(self._data[self._data["sample"].str.contains("GJets") == True])
-		print("sig",sig,"nom",nom,"gmsb",gmsb,"gjets",gjets)
+		qcd = len(self._data[self._data["sample"].str.contains("QCD") == True])
+		d = len(self._data[self._data["sample"] == "METPD"]) + len(self._data[self._data["sample"] == "EGamma"]) 
+		tot = len(self._data)
+		print(" ",tot, ("subclusters, data: "+str(d)+" {:.2f}%, GJets: "+str(gjets)+" {:.2f}%, QCD "+str(qcd)+" {:.2f}%, GMSB "+str(gmsb)+" {:.2f}%").format(d/tot,gjets/tot,qcd/tot,gmsb/tot))
+		
 		#remove not-signal-matched photons in GMSB sample (this is not the "bkg" we want to target)
+		print("after GMSB bkg removal")
 		rowbool = ((self._data["sample"].str.contains("GMSB") == True) & (self._data["label"] == 1)) 
 		self._data = self._data.drop(self._data[rowbool].index)	 
-		print("after GMSB bkg removal",len(self._data))
 		sig = len(self._data[self._data["label"] == 0])
 		nom = len(self._data[self._data["label"] == 1])
 		gmsb = len(self._data[self._data["sample"].str.contains("GMSB") == True])
 		gjets = len(self._data[self._data["sample"].str.contains("GJets") == True])
-		print("sig",sig,"nom",nom,"gmsb",gmsb,"gjets",gjets)
+		qcd = len(self._data[self._data["sample"].str.contains("QCD") == True])
+		d = len(self._data[self._data["sample"] == "METPD"]) + len(self._data[self._data["sample"] == "EGamma"]) 
+		tot = len(self._data)
+		print(" ",tot, ("subclusters, data: "+str(d)+" {:.2f}%, GJets: "+str(gjets)+" {:.2f}%, QCD "+str(qcd)+" {:.2f}%, GMSB "+str(gmsb)+" {:.2f}%").format(d/tot,gjets/tot,qcd/tot,gmsb/tot))
+		
 		#put extra cuts on subcluster energy, etc.	
 		#remove nans - TODO: see why this is happening with minor_length
 		self._data.dropna()
@@ -43,7 +51,10 @@ class CSVReader:
 		nom = len(self._data[self._data["label"] == 1])
 		gmsb = len(self._data[self._data["sample"].str.contains("GMSB") == True])
 		gjets = len(self._data[self._data["sample"].str.contains("GJets") == True])
-		print("sig",sig,"nom",nom,"gmsb",gmsb,"gjets",gjets)
+		qcd = len(self._data[self._data["sample"].str.contains("QCD") == True])
+		d = len(self._data[self._data["sample"] == "METPD"]) + len(self._data[self._data["sample"] == "EGamma"]) 
+		tot = len(self._data)
+		print(" ",tot, ("subclusters, data: "+str(d)+" {:.2f}%, GJets: "+str(gjets)+" {:.2f}%, QCD "+str(qcd)+" {:.2f}%, GMSB "+str(gmsb)+" {:.2f}%").format(d/tot,gjets/tot,qcd/tot,gmsb/tot))
 				
 	def GetData(self):
 		return self._data
