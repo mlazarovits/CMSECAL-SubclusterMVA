1fba968b98563e4938afa5c6698a948b7b21247d
Margaret Lazarovits <mlazarovits@gmail.com>
2024-11-25 11:49:12 -0600

    added one-hot encoding to labels in TransformLabels()

--- ProcessData.py
+++ ProcessData.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import LabelBinarizer
 
 #add multiple files
 class CSVReader:
@@ -14,6 +15,7 @@ class CSVReader:
 		#data is a list of feature, label pairs or tuples
 		self._data = np.array([])
 		self._header = np.array([])
+		self._data = pd.DataFrame()
 
 	def AddFile(self,file):
 		data = pd.read_csv(file)
@@ -31,24 +33,52 @@ class CSVReader:
 	
 		
 	#data cleaning, cuts, etc.
-	def CleanData(self):
+	#unmatched = -1
+	#signal = 0
+	#!signal = 1 
+	#BH = 2
+	#spike = 3
+	def CleanData(self, printStats = False):
 		print("Cleaning data",len(self._data),"subclusters initially")
 		#remove any "unmatched" labels
-		print("after unmatched removal")
 		self._data = self._data[self._data['label'] != -1]
-		self.PrintStats()
+		if(printStats):
+			print("after unmatched removal")
+			self.PrintStats()
 	
 		#remove not-signal-matched photons in GMSB sample (this is not the "bkg" we want to target)
-		print("after GMSB bkg removal")
 		rowbool = ((self._data["sample"].str.contains("GMSB") == True) & (self._data["label"] == 1)) 
 		self._data = self._data.drop(self._data[rowbool].index)	 
-		self.PrintStats()
+		if(printStats):
+			print("after GMSB bkg removal")
+			self.PrintStats()
+
+		#remove phys entries from data samples - want these entries to come from MC
+		rowbool = ((self._data["sample"].str.contains("METPD") == True) & (self._data["label"] == 1)) 
+		self._data = self._data.drop(self._data[rowbool].index)	 
+		rowbool = ((self._data["sample"].str.contains("JetHT") == True) & (self._data["label"] == 1)) 
+		self._data = self._data.drop(self._data[rowbool].index)	 
+		rowbool = ((self._data["sample"].str.contains("EGamma") == True) & (self._data["label"] == 1)) 
+		self._data = self._data.drop(self._data[rowbool].index)	 
+		if(printStats):
+			print("after data bkg removal")
+			self.PrintStats()
 		
 		#put extra cuts on subcluster energy, etc.	
 		self._data.dropna()
-		print('after dropna')
-		self.PrintStats()
+		if(printStats):
+			print('after dropna')
+			self.PrintStats()
+
+
+	def TransformLabels(self):
+		lb = LabelBinarizer()
+		labels = self._data["label"]
+		newlabels = lb.fit_transform(labels)
+		self._data = self._data.assign(label = pd.Series(list(newlabels)).values)
+		print(self._data["label"])
 	
+
 	def GetData(self):
 		return self._data
 
