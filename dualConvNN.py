810abd24387b88d3a72c512e1341d5c507badc4f
Margaret Lazarovits <mlazarovits@gmail.com>
2024-12-18 10:32:19 -0800

    udpated masks for dual CNN to be configurable from BuildModel()

--- dualConvNN.py
+++ dualConvNN.py
@@ -8,31 +8,31 @@ class dualConvNeuralNetwork(ConvNeuralNetwork):
 	def __init__(self, data, nNodes, name = "model",channels = ["E","t","r"]):
 		super().__init__(data, nNodes, name, channels)
 
-	#dual conv NNs with different masks - one for spikes, one for beam halo
-	def BuildModel(self):
+	#dual conv NNs with different masks - both are tuples
+	def BuildModel(self, mask1, mask2):
 		input_layer = Input(shape=self._xtrain[0].shape)
 	
-		#n filters with 3x3 kernels - spikes
-		kernel_dim_spikes = 3
+		#first CNN
+		kernel_dim_1 = mask1
 		#reLu activation at internal layers
-		conv_layers_spikes = [layers.Conv2D(n,kernel_dim_spikes,name="spike_conv_layer"+str(i),activation=activations.relu) for i, n in enumerate(self._nNodes)]
-		x_spikes = conv_layers_spikes[0](input_layer)
-		for i, c in enumerate(conv_layers_spikes[1:]):
-			x_spikes = c(x_spikes)
+		conv_layers_1 = [layers.Conv2D(n,kernel_dim_1,name="first_conv_layer"+str(i),activation=activations.relu) for i, n in enumerate(self._nNodes)]
+		x_1 = conv_layers_1[0](input_layer)
+		for i, c in enumerate(conv_layers_1[1:]):
+			x_1 = c(x_1)
 
-		#n filters with 5x3 kernels - spikes
-		kernel_dim_bh = (3,1)
+		#second CNN
+		kernel_dim_2 = mask2
 		#reLu activation at internal layers
-		conv_layers_bh = [layers.Conv2D(n,kernel_dim_bh,name="bh_conv_layer"+str(i),activation=activations.relu) for i, n in enumerate(self._nNodes)]
-		x_bh = conv_layers_bh[0](input_layer)
-		for i, c in enumerate(conv_layers_bh[1:]):
-			x_bh = c(x_bh)
+		conv_layers_2 = [layers.Conv2D(n,kernel_dim_2,name="second_conv_layer"+str(i),activation=activations.relu) for i, n in enumerate(self._nNodes)]
+		x_2 = conv_layers_2[0](input_layer)
+		for i, c in enumerate(conv_layers_2[1:]):
+			x_2 = c(x_2)
 	
 		#put into (None, 1, 1, nNodes) shape
-		kernel_dim_bh = (x_bh.shape[1], x_bh.shape[2])
-		x_bh = layers.Conv2D(self._nNodes[-1],kernel_dim_bh,name="bh_conv_layer_last",activation=activations.relu)(x_bh)
+		kernel_dim_2 = (x_2.shape[1], x_2.shape[2])
+		x_2 = layers.Conv2D(self._nNodes[-1],kernel_dim_2,name="second_conv_layer_last",activation=activations.relu)(x_2)
 
-		x = layers.concatenate([x_spikes, x_bh])	
+		x = layers.concatenate([x_spikes, x_2])	
 
 		#flatten from 2D to 1D
 		x = layers.Flatten()(x)
