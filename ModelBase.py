04793a184011923d88ca5f23e2ce8668a660a88c
Margaret Lazarovits <mlazarovits@gmail.com>
2025-01-14 14:03:25 -0600

    removed double VizMulticlassROC method, added printout for FPR at ~1% 1-TPR (misid rate)

--- ModelBase.py
+++ ModelBase.py
@@ -45,7 +45,7 @@ class ModelBase(ABC):
 		plt.figure()
 		plt.plot(history.history['val_'+fname], label="val "+fname)
 		plt.plot(history.history[fname],label="train "+fname)
-		plt.title(self._name+"\n"+fname)
+		plt.title(self._name+"\n"+fname,fontsize=10)
 		plt.legend()
 		#check if output dir exists
 		print("Saving loss plot to",self._path+"/"+fname+"."+self._form)
@@ -122,9 +122,7 @@ class ModelBase(ABC):
 				else:
 					paircolors[(cat1,cat2)] = self._catcolors[cat2]
 			for idx, (cat1, cat2) in enumerate(pairs):
-				title=self._name+"\n"+self._catnames[cat1]+" vs "+self._catnames[cat2]+" subcluster ROC"
 				#y_test needs to be categorical labels
-				#print("cat1",cat1,self._catnames[cat1],"cat2",cat2,self._catnames[cat2])
 				cat1_mask = ytrue_cat == cat1
 				cat2_mask = ytrue_cat == cat2
 				cat12_mask = np.logical_or(cat1_mask, cat2_mask)
@@ -143,7 +141,18 @@ class ModelBase(ABC):
 				tpr_cat1 = [1 - i for i in tpr_cat1]
 				if min(tpr_cat1[:-2]) < ymin:
 					ymin = min(tpr_cat1[:-2])
-				
+	
+				#get FPR for 1-tpr (misid) ~ 0.01
+				#find index of entry in tpr for element that is closest to 0.01
+				mindiff = 999
+				bestIdx = 0
+				for i, tpr in enumerate(tpr_cat1):
+					diff = abs(tpr - 0.01)
+					if diff < mindiff:
+						mindiff = diff
+						bestIdx = i
+				print("cat1",cat1,self._catnames[cat1],"cat2",cat2,self._catnames[cat2],"fpr",fpr_cat1[bestIdx],"tpr",tpr_cat1[bestIdx])
+					
 				ax.plot(
 					fpr_cat1,
 					tpr_cat1,
@@ -172,109 +181,7 @@ class ModelBase(ABC):
 		else:
 			ax.set_ylim([1e-6,1.])
 			ax.set_xlim([0,1])
-		ax.set(
-			xlabel="FPR",
-			ylabel="1 - TPR (misid rate)",
-			title=title
-		)
-			#line, = ax.plot([0, 1], [0, 1], "k--", label="Chance level (AUC = 0.5)")
-		print("Saving ROC plot to",self._path+"/ROC_"+fname+"."+self._form)
-		plt.savefig(self._path+"/ROC_"+fname+"."+self._form,format=self._form)
-		plt.close()
-
-	#ytrue and ypred are given in onehot form	
-	#if cat = -1, plot one vs one for all classes
-	#if cat != -1, plot cat vs all
-	def VizMulticlassROC(self, ytrue, ypred, cat = -1, zoom = False):
-		title=""
-
-		ymin = 999
-		fig = plt.figure()
-		ax = plt.gca()
-		#one vs all
-		if cat != -1:
-			catname = self._catnames[cat] 
-			col = "pink" #also get from dict?
-			title=self._name+"\n"+catname+" vs all subcluster ROC"
-			fname = catname+"_vs_all"
-			
-			fpr, tpr, thresh = roc_curve(ytrue[:,cat], ypred[:, cat])
-			
-			#do 1- TPR
-			tpr = [1 - i for i in tpr]
-			if min(tpr[:-2]) < ymin:
-				ymin = min(tpr[:-2])
-			ax.plot(
-				fpr,
-				tpr,
-				linewidth=4,
-				label=catname+" vs rest",
-				color=col,
-			)
-		#do all one vs ones
-		else:
-			title = "one vs one ROC"
-			#make unique pairs of categories
-			ytrue_cat = self._lb.inverse_transform(ytrue)
-			pairs = list(combinations(np.unique(ytrue_cat), 2))
-			fname = "one_vs_ones"
-			paircolors = {}
-			for (cat1, cat2) in pairs:
-				if self._catcolors[cat1] not in paircolors.values():
-					paircolors[(cat1,cat2)] = self._catcolors[cat1]
-				else:
-					paircolors[(cat1,cat2)] = self._catcolors[cat2]
-			for idx, (cat1, cat2) in enumerate(pairs):
-				title=self._name+"\n"+self._catnames[cat1]+" vs "+self._catnames[cat2]+" subcluster ROC"
-				#y_test needs to be categorical labels
-				#print("cat1",cat1,self._catnames[cat1],"cat2",cat2,self._catnames[cat2])
-				cat1_mask = ytrue_cat == cat1
-				cat2_mask = ytrue_cat == cat2
-				cat12_mask = np.logical_or(cat1_mask, cat2_mask)
-
-				cat1_true = cat1_mask[cat12_mask]
-				cat2_true = cat2_mask[cat12_mask]
-							
-				#get indices of categories in one-hot labels
-				idx1 = np.flatnonzero(self._lb.classes_ == cat1)[0]
-				idx2 = np.flatnonzero(self._lb.classes_ == cat2)[0]
-
-				fpr_cat1, tpr_cat1, thresh_cat1 = roc_curve(cat1_true, ypred[cat12_mask, idx1])
-				fpr_cat2, tpr_cat2, thresh_cat2 = roc_curve(cat2_true, ypred[cat12_mask, idx2])
-				
-				#do 1- TPR
-				tpr_cat1 = [1 - i for i in tpr_cat1]
-				if min(tpr_cat1[:-2]) < ymin:
-					ymin = min(tpr_cat1[:-2])
-				
-				ax.plot(
-					fpr_cat1,
-					tpr_cat1,
-					linewidth=4,
-					label=self._catnames[cat1]+" vs "+self._catnames[cat2],
-					color=paircolors[(cat1,cat2)],
-				)
-				'''
-				RocCurveDisplay.from_predictions(
-					cat1_true,
-					ypred[cat12_mask, idx1],
-					name=self._catnames[cat1]+" vs "+self._catnames[cat2],
-					color=paircolors[(cat1,cat2)],
-					ax=ax,
-					pos_label = 1
-    				)
-				'''
-			ax.legend()
-		ax.set_yscale('log')	
-		ax.grid()
-		#focus on discriminating region of interest
-		if(zoom):
-			ax.set_ylim([1e-2, 5e-1])
-			ax.set_xlim([0,0.1])
-			fname += "_zoom"
-		else:
-			ax.set_ylim([1e-6,1.])
-			ax.set_xlim([0,1])
+		title=self._name+"\n 1-v-1 ROC"
 		ax.set(
 			xlabel="FPR",
 			ylabel="1 - TPR (misid rate)",
