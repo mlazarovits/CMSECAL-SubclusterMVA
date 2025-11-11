7ec464b38a30794daadec4c6539b04acc2a8ca3b
Margaret Lazarovits <mlazarovits@gmail.com>
2025-11-11 09:41:17 -0600

    dividing isolation by pt

--- runKUDNN_photonID.py
+++ runKUDNN_photonID.py
@@ -1,25 +1,42 @@
 import argparse
 from ProcessData import CSVReader
 import pandas as pd
+import numpy as np
 from DeepNN import DeepNeuralNetwork
 
 # DNN for bkg classification (iso vs noniso) 
 def runDNN(args):
 	#data
-	reader = CSVReader()
-	#reader.AddFile("csv/MET_R17_AL1IsoPho_v22_MET_AOD_Run2017E_17Nov2017_superclusters_defaultv4.csv")
-	#reader.AddFile("csv/DEG_R17_AL1IsoPho_v22_DoubleEG_AOD_Run2017F_09Aug2019_UL2017_superclusters_defaultv3p5.csv")
-	#reader.AddFile("csv/JetHT_R17_AL1IsoPho_v22_JetHT_AOD_Run2017F_17Nov2017_superclusters_defaultv3p5.csv")
+	printstats = True
+	reader = CSVReader(printstats)
 	reader.AddFile("csv/GJets_R18_InvMetPho30_v31_GJets_HT-400To600_TuneCP5_AODSIM_RunIISummer20UL18RECO_photons_defaultv3p10_noIso_isoBkgSel_beta0-1e-5_m0-0p0-0p0-0p0_W0diag-0p013-0p013-33p333_nu0-3_NperGeV-0p0333333_emAlpha-1e-5.csv")
 	reader.AddFile("csv/QCD_R18_InvMET100_v31_QCD_HT300to500_TuneCP5_AODSIM_RunIISummer20UL18RECO_photons_defaultv3p10_noIso_beta0-1e-5_m0-0p0-0p0-0p0_W0diag-0p013-0p013-33p333_nu0-3_NperGeV-0p0333333_emAlpha-1e-5.csv")	
+
 	
 	reader.CleanData()
-	#cap energy of isolated bkg to remove dependence
-	if(args.reweightClasses):
-		reader.ReweightClasses(6,4,"energy")
-		if(args.extra is None):
-			args.extra = "reweighted"
+	reader.MakeSigmas(['EtaVar','PhiVar'])
+	shape_cols = ["EtaSig","PhiSig","EtaPhiCov","majorLength", "minorLength"]
+	iso_cols = ["hcalTowerSumEtConeDR04","trkSumPtSolidConeDR04","trkSumPtHollowConeDR04","hadTowOverEM","ecalRHSumEtConeDR04"]
+	#create new columns of relative isolation
+	reader.DivideCols(iso_cols,'Pt')
+	iso_cols = [i+"OvPt" for i in iso_cols]
+	reader.BalanceClasses([4,6])
 	data = reader.GetData()
+
+
+	#process SMS data - will add test nonisobkg data after train_test_split in DeepNN ctor
+	SMSreader = CSVReader(printstats)
+	SMSreader.AddFile("csv/SMS_Sig_SVIPM100_v31_SMS-GlGl_AODSIM_mGl-2000_mN2-1500_mN1-500_photons_defaultv3p10_noIso_beta0-1e-5_m0-0p0-0p0-0p0_W0diag-0p013-0p013-33p333_nu0-3_NperGeV-0p0333333_emAlpha-1e-5.csv")
+	SMSreader.MakeSigmas(['EtaVar','PhiVar'])
+	#only want photons from n2 (label 0) and non iso bkg (label 6)
+	SMSreader.RemoveEntries('label',1)
+	SMSreader.RemoveEntries('label',5)
+	#switch label 0 to label 4 to act as 'iso bkg' sig
+	SMSreader.SetFeatureFromValToVal('label',0,4)
+	SMSreader.CleanData()
+	#SMSreader.CapClass(4,3000)
+	#SMSreader.BalanceClasses([4,6])
+	sms_data = SMSreader.GetData()
 
 	#labels
 	#unmatched = -1
@@ -36,83 +53,23 @@ def runDNN(args):
 	
 	catToName = {4 : "isoBkg", 6 : "nonIsoBkg"}
 	catToColor = {4 : "green", 6 : "red"}
-	tot = len(data)
-	isobkg = len(data[data["label"] == 4])
-	nonisobkg = len(data[data["label"] == 6])
-	print(" ",tot, ("subclusters, iso bkg: "+str(isobkg)+" {:.2f}%, noniso bkg: "+str(nonisobkg)+" {:.2f}%").format(isobkg/tot,nonisobkg/tot))
-	#need to viz inputs before transforming labels bc relies on integer labels
 
-	'''
-	#by process breakdown
-	print("\nMET sample")
-	METreader.CleanData()
-	data = METreader.GetData()
-	tot = len(data)
-	phys = len(data[data["label"] == 1])
-	BH = len(data[data["label"] == 2])
-	spike = len(data[data["label"] == 3])
-	print(" ",tot, ("subclusters, phys: "+str(phys)+" {:.2f}%, spike: "+str(spike)+" {:.2f}%, BH: "+str(BH)+" {:.2f}%").format(phys/tot,spike/tot,BH/tot))
-	
-	print("DoubleEG sample")
-	DEGreader.CleanData()
-	data = DEGreader.GetData()
-	tot = len(data)
-	phys = len(data[data["label"] == 1])
-	BH = len(data[data["label"] == 2])
-	spike = len(data[data["label"] == 3])
-	print(" ",tot, ("subclusters, phys: "+str(phys)+" {:.2f}%, spike: "+str(spike)+" {:.2f}%, BH: "+str(BH)+" {:.2f}%").format(phys/tot,spike/tot,BH/tot))
-	
-	print("JetHT sample")
-	JetHTreader.CleanData()
-	data = JetHTreader.GetData()
-	tot = len(data)
-	phys = len(data[data["label"] == 1])
-	BH = len(data[data["label"] == 2])
-	spike = len(data[data["label"] == 3])
-	print(" ",tot, ("subclusters, phys: "+str(phys)+" {:.2f}%, spike: "+str(spike)+" {:.2f}%, BH: "+str(BH)+" {:.2f}%").format(phys/tot,spike/tot,BH/tot))
-	'''
-
-	#randomly select # sig == # nom entries
-	print("Even out classes")
-	sizes = {}
-	sizes[len(data[data["label"] == 4])] = 4
-	sizes[len(data[data["label"] == 6])] = 6
-
-	print(sizes)
-	nsamp = min(sizes.keys())
-	lab = sizes[nsamp] #label of min sample
-	
-	data_samples = []	
-	for l in sizes.values():
-		if l == lab:
-			continue	
-		data_samples.append(data.query('label == '+str(l)).sample(n=nsamp,random_state=111))
-	data_samples.append(data.query('label == '+str(lab)))
-	data = pd.concat(data_samples)
-	tot = len(data)
-	isobkg = len(data[data["label"] == 4])
-	nonisobkg = len(data[data["label"] == 6])
-	print(" ",tot, ("subclusters, iso bkg: "+str(isobkg)+" {:.2f}%, noniso bkg: "+str(nonisobkg)+" {:.2f}%").format(isobkg/tot,nonisobkg/tot))
-
-
-		
+	default_cols = ["sample","event","object","label"]
 	network_name = "KU-DNN_photonID"
 	if args.extra is not None:
 		network_name += "_"+args.extra
 	nepochs = int(args.nEpochs)
 	early = False
-	shape_cols = ["sample","event","object","subcl","eta_sig","phi_sig","etaphi_cov","major_length", "minor_length","label"]
-	iso_cols = ["sample","event","object","subcl","trkSumPtSolidConeDR04","hadTowOverEM","ecalRHSumEtConeDR04","label"]
 	if(args.network == "shape"):
 		#default input set
+		shape_cols += default_cols
 		cols = shape_cols 
 	elif(args.network == "iso"):
 		#default input set
+		iso_cols += default_cols
 		cols = iso_cols 
 	elif(args.network == "isoShape"):
-		cols = shape_cols + iso_cols
-		cols = set(cols)
-		cols = list(cols) 
+		cols = default_cols + shape_cols + iso_cols
 	else:
 		print("Invalid network selected",args.network)
 	network_name += "_"+args.network
@@ -128,12 +85,11 @@ def runDNN(args):
 			network_name += "_excludingFeature_"+args.exclude
 
 	if(args.dryRun):
-		cols.append("energy")
-	if(args.reweightClasses):
-		cols.append("weight")
+		cols.append("Energy")
+	#if(args.reweightClasses):
+	#	cols.append("weight")
 
 	print("features used",cols)	
-	data = data[cols]
 	network_name += "_"+str(nepochs)+"epochs"
 	if(early):
 		network_name += "_earlyStop"
@@ -152,15 +108,14 @@ def runDNN(args):
 		nodes = [8, 8] 
 	
 	
-	model = DeepNeuralNetwork(data,nodes,network_name)
+	model = DeepNeuralNetwork(data,nodes,cols,catToName,catToColor,network_name)
 	model.BuildModel()
-	model.SetCategoryNames(catToName,catToColor)
 	if(args.testNetwork):
-	    print("Evaluating network",network_name)
-	    model.TestModel(1,True)
+	    print("Evaluating network on test sample",network_name)
+	    model.TestModel(1,1)
+	    print("Evaluating network on external signal sample",network_name)
+	    model.TestModel_ExternalSignal(sms_data,cols)
 	    return
-	model.VizInputs()
-	#visualize inputs
 	model.CompileModel()
 	model.summary()
 	if(args.dryRun):
@@ -168,7 +123,10 @@ def runDNN(args):
 	#input is TrainModel(epochs=1,oname="",int:verb=1)
 	model.TrainModel(nepochs,batch=100,viz=True,savebest=True,earlystop=early)
 	#needs test data + to make ROC plots
-	model.TestModel(1,True,validate_model=True)
+	print("Evaluating network on test sample",network_name)
+	model.TestModel(1,1,validate_model=True)
+	print("Evaluating network on external signal sample",network_name)
+	model.TestModel_ExternalSignal(sms_data,cols)
 
 
 def main():
@@ -179,7 +137,7 @@ def main():
 	parser.add_argument("--dryRun",help="dry run - stats only (don't run network)",action='store_true',default=False)
 	parser.add_argument("--extra",'-e',help='extra string for network name')
 	parser.add_argument("--exclude",help='exclude feature from training',default=None)
-	parser.add_argument("--reweightClasses",help="reweight classes",default=False,action='store_true')
+	#parser.add_argument("--reweightClasses",help="reweight classes",default=False,action='store_true')
 	parser.add_argument('--testNetwork',help='evaluate trained network specified by other flags',default=False,action='store_true')
 	args = parser.parse_args()
 
