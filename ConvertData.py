1c6736b40826cb3ce3d7be34c724953248304de5
Margaret Lazarovits <mlazarovits@gmail.com>
2025-12-12 16:38:49 -0600

    debugged hanging for ProcessDNNBranches - needed to update chunk # if skipping file

--- ConvertData.py
+++ ConvertData.py
@@ -32,7 +32,7 @@ class TTreeReader(FileReader):
 		self._output_parquet_data = self._output_parquet_data+f"/{self._tag}_{self._obj}s"
 		os.makedirs(self._output_parquet_data,exist_ok=True)
 
-	def ProcessCNNBranches(self, file, sample, step_size=10000, maxnchunk = -1, debug = False, recreate_files = False):
+	def ProcessCNNBranches(self, file, sample, step_size=10000, maxnchunk = -1, debug = False, labelas = -999, recreate_files = False):
 		branches = [
 			f"SC_rh_iEta_{self._tag}",
 			f"SC_rh_iPhi_{self._tag}",
@@ -41,19 +41,21 @@ class TTreeReader(FileReader):
 			f"SC_EtaCenter_{self._tag}",
 			f"SC_seedTime_CMS"
 		]
-		print("Branches",branches)
 		data_accum = []
 		nchunk = 0
 		total_time = 0
 
 		if(isinstance(step_size,int) and maxnchunk == -1):
-			print("Processing sample",sample,"from file",file,"with step size",step_size)
 			nentries = uproot.open(file)["tree"].num_entries
-			print("File",file,"has",nentries,"entries and therefore",(nentries + step_size - 1) // step_size,"chunks with step size",step_size)
+			nchunks = (nentries + step_size - 1) // step_size
 		else:
-			print("Chunking file",file,"in",maxnchunk,"chunks with step size",step_size)
-		
-		
+			nchunks = maxnchunk+1
+		ncurrent_files = len([name for name in os.listdir(self._output_parquet_data) if f"sample_{sample}_type_{self._tag}.parquet" in name])
+		if os.path.exists(self._output_parquet_data) and not recreate_files and ncurrent_files == nchunks:
+			print(nchunks,"parquet files already processed for file",file,"in",self._output_parquet_data,"returning\n")
+			return
+		print("Processing sample",sample,"from file",file,"with step size",step_size,"and",nchunks,"chunks")
+		print("Branches",branches)
 		for chunk in uproot.iterate(file + ":tree", branches, step_size=step_size, library="ak",
 				num_workers = 8, #multithreading options
 				decompression_executor=ThreadPoolExecutor(max_workers=8),
@@ -73,13 +75,16 @@ class TTreeReader(FileReader):
 			print(f"Processing chunk #{nchunk}",end="\r",flush=True)
 			
 			sc_counts = ak.num(chunk[f"SC_trueLabel_{self._tag}"])
+			truelabel = ak.to_numpy(ak.flatten(chunk[f"SC_trueLabel_{self._tag}"],axis=1))
+			if labelas != -999:
+				truelabel = pa.array([labelas] * sum(sc_counts))
 			#write to parquet table directly
 			table = pa.table({
 				"event_idx": np.repeat(np.arange(len(chunk)), sc_counts),
 				"sc_idx": ak.to_numpy(ak.flatten(ak.local_index(chunk[f"SC_trueLabel_{self._tag}"]))),
 				f"SC_EtaCenter_{self._tag}" : ak.to_numpy(ak.flatten(chunk[f"SC_EtaCenter_{self._tag}"],axis=1)),
 				f"SC_seedTime_CMS" : ak.to_numpy(ak.flatten(chunk[f"SC_seedTime_CMS"],axis=1)), #ak.to_numpy must be flat arrays
-				f"SC_trueLabel_{self._tag}" : ak.to_numpy(ak.flatten(chunk[f"SC_trueLabel_{self._tag}"],axis=1)),
+				f"SC_trueLabel_{self._tag}" : truelabel,
 				f"SC_rh_iEta_{self._tag}" : ak.to_list(ak.flatten(chunk[f"SC_rh_iEta_{self._tag}"], axis=1)), #ak.to_list can be jagged arrays
 				f"SC_rh_iPhi_{self._tag}" : ak.to_list(ak.flatten(chunk[f"SC_rh_iPhi_{self._tag}"], axis=1)),
 				f"SC_rh_Energy_{self._tag}" : ak.to_list(ak.flatten(chunk[f"SC_rh_Energy_{self._tag}"], axis=1)),
@@ -100,12 +105,12 @@ class TTreeReader(FileReader):
 		#self._data = pd.concat(data_accum, ignore_index=True)
 		print("Done processing file",file,"took",total_time,"seconds total with",total_time / nchunk,"seconds on average per chunk\n\n")
 	
-	def ProcessFileCNN(self, file, sample, step_size=10000, maxnchunk = -1, debug=False):
-		self.ProcessCNNBranches(file,sample,step_size,maxnchunk,debug)
+	def ProcessFileCNN(self, file, sample, step_size=10000, maxnchunk = -1, debug=False, labelas = -999):
+		self.ProcessCNNBranches(file,sample,step_size,maxnchunk,debug,labelas)
 
-	def ProcessFileDNN(self, file, sample, step_size=10000, maxnchunk = -1, debug=False):
+	def ProcessFileDNN(self, file, sample, step_size=10000, maxnchunk = -1, debug=False, labelas = -999):
 		self.ProcessDNNBranches(file,sample,step_size, maxnchunk)
-
+		print("Wrote parquet chunks to",self._output_parquet_data)
 	
 	def ProcessDNNBranches(self, file, sample, step_size=10000,  maxnchunk = -1,debug = False):
 		branches = [
@@ -121,8 +126,7 @@ class TTreeReader(FileReader):
 			f"Photon_ecalRHSumEtConeDR04",
 			f"Photon_Pt_{self._tag}",
 			f"Photon_EtaCenter_{self._tag}",
-			f"Photon_trueLabel_{self._tag}"
-			
+			f"Photon_trueLabel_{self._tag}",
 		]
 		print("Branches",branches)
 		data_accum = []
@@ -130,7 +134,8 @@ class TTreeReader(FileReader):
 		total_time = 0
 
 		print("Processing sample",sample,"from file",file,"with step size",step_size)
-		nentries = uproot.open(file)["tree"].num_entries
+		tree = uproot.open(file)["tree"]
+		nentries = tree.num_entries
 		if(isinstance(step_size,int)):
 			print("File",file,"has",nentries,"entries and therefore",(nentries + step_size - 1) // step_size,"chunks with step size",step_size)
 		else:
@@ -145,15 +150,17 @@ class TTreeReader(FileReader):
 			if(os.path.exists( os.path.join(self._output_parquet_data, parquet_fname) )):
 				#print(os.path.join(self._output_parquet_data, parquet_fname),"exists. Please provide a unique sample name for",file)
 				#print(os.path.join(self._output_parquet_data, parquet_fname),"exists. Skipping.",end="\r",flush=True)
+				print(f"Skipping chunk #{nchunk}",end="\r",flush=True)
+				nchunk += 1
 				continue
 			if nchunk > 2 and debug:
 				print("Break from debugging")
 				return
-			if maxnchunk != -1 and nchunk > maxnchunk:
+			if maxnchunk != -1 and nchunk == maxnchunk:
 				break
 			t1 = time.perf_counter()
 			print(f"Processing chunk #{nchunk}",end="\r",flush=True)
-			
+	
 			pho_counts = ak.num(chunk[f"Photon_trueLabel_{self._tag}"])
 			#write to parquet table directly
 			table = pa.table({
@@ -182,8 +189,10 @@ class TTreeReader(FileReader):
 			t2 = time.perf_counter()
 			total_time += (t2 - t1)
 			print(f"Chunk #{nchunk} processed took",(t2-t1),"seconds",end="\r",flush = True)
-		
-		print("Done processing file",file,"took",total_time,"seconds total with",total_time / nchunk,"seconds on average per chunk\n\n")
+		if nchunk == 0:
+			print("Ran over 0 chunks")
+		else:
+			print("Done processing file",file,"took",total_time,"seconds total with",total_time / nchunk,"seconds on average per chunk\n\n")
 	
 
 	def ReadDataFromParquetTable(self):
