236584ec732a3ecf93462b52dd015dab09e16e45
Margaret Lazarovits <mlazarovits@gmail.com>
2025-12-18 19:37:10 -0600

    update

--- runKUDNN_photonID_dask.py
+++ runKUDNN_photonID_dask.py
@@ -1,44 +1,30 @@
 import argparse
-from ProcessData import CSVReader
+from ProcessData import DataCleaner 
 import pandas as pd
-import numpy as np
 from DeepNN import DeepNeuralNetwork
+import numpy as np
 
 # DNN for bkg classification (iso vs noniso) 
 def runDNN(args):
-	#data
 	printstats = True
-	reader = TTreeReader("Photon",printstats)
-	reader.AddFileDNN("")
+	cleaner = DataCleaner(args.parquetpath, "Photon", "CMS", printstats)
+	dask_df = cleaner.GetDaskData(debug=args.debug)
+	cleaner.CleanAndConvert(dask_df)
+	cleaner.SelectClass(4,"EGamma")
+	cleaner.SelectClass(6,"JetHT")
+	cleaner.BarrelOnly("Photon_EtaCenter")	
+	classes_to_balance = [4,6]
+	cleaner.BalanceClasses(classes_to_balance)
 	
-	shape_cols = ["EtaSig","PhiSig","EtaPhiCov","majorLength", "minorLength"]
-	iso_cols = ["hcalTowerSumEtConeDR04","trkSumPtSolidConeDR04","trkSumPtHollowConeDR04","hadTowOverEM","ecalRHSumEtConeDR04"]
-	reader.CleanDataDask()
-	reader.BarrelOnly("Photon_EtaCenter")
-	reader.BalanceClasses([4,6])
-	reader.MakeSigmas(['EtaVar','PhiVar'])
+	shape_cols = ["Photon_EtaSig_CMS","Photon_PhiSig_CMS","Photon_EtaPhiCov_CMS","Photon_majorLength_CMS", "Photon_minorLength_CMS"]
+	iso_cols = ["Photon_hcalTowerSumEtConeDR04","Photon_trkSumPtSolidConeDR04","Photon_trkSumPtHollowConeDR04","Photon_hadTowOverEM","Photon_ecalRHSumEtConeDR04"]
+	#reader.BalanceClasses([4,6])
+	#create sigma columns
+	cleaner.MakeSigmas(['Photon_EtaVar_CMS','Photon_PhiVar_CMS'])
 	#create new columns of relative isolation
-	iso_cols_pt = reader.DivideCols(iso_cols,'Pt')
-	data = reader.GetData()
-
-	'''
-	#process SMS data - will add test nonisobkg data after train_test_split in DeepNN ctor
-	SMSreader = CSVReader(printstats)
-	SMSreader.AddFile("csv/SMS_Sig_SVIPM100_v31_SMS-GlGl_AODSIM_mGl-2000_mN2-1500_mN1-500_photons_defaultv3p10_noIso_beta0-1e-5_m0-0p0-0p0-0p0_W0diag-0p013-0p013-33p333_nu0-3_NperGeV-0p0333333_emAlpha-1e-5.csv")
-	SMSreader.MakeSigmas(['EtaVar','PhiVar'])
-	SMSreader.DivideCols(iso_cols,'Pt')
-	#only want photons from n2 (label 0) and non iso bkg (label 6)
-	SMSreader.RemoveEntries('label',1)
-	SMSreader.RemoveEntries('label',5)
-	#switch label 0 to label 4 to act as 'iso bkg' sig
-	SMSreader.SetFeatureFromValToVal('label',0,4)
-	SMSreader.BarrelOnly()
-	SMSreader.CleanData()
-	#SMSreader.CapClass(4,3000)
-	#SMSreader.BalanceClasses([4,6])
-	sms_data = SMSreader.GetData()
-	'''
+	iso_cols_pt = cleaner.DivideCols(iso_cols,'Photon_Pt_CMS')
 	iso_cols = iso_cols_pt
+	data = cleaner.GetData()
 
 	#labels
 	#unmatched = -1
@@ -102,45 +88,49 @@ def runDNN(args):
 	network_name += "_"+args.arch
 	if(args.arch == "default"):
 		nodes = [64, 64, 64]
+	if(args.arch == "med32"):
+		nodes = [32, 32, 32] 
 	if(args.arch == "med16"):
 		nodes = [16, 16, 16] 
 	if(args.arch == "med8"):
 		nodes = [8, 8, 8] 
+	if(args.arch == "large8"):
+		nodes = [8, 8, 8, 8, 8] 
 	if(args.arch == "small8"):
 		nodes = [8, 8] 
 	
 	
 	model = DeepNeuralNetwork(data,nodes,cols,catToName,catToColor,network_name)
+	model.VizInputs()
 	model.BuildModel()
-	if(args.testNetwork):
-	    print("Evaluating network on test sample",network_name)
-	    model.TestModel(1,1)
-	    print("Evaluating network on external signal sample",network_name)
-	    model.TestModel_ExternalSignal(sms_data,cols)
-	    return
 	model.CompileModel()
 	model.summary()
+	if(args.testNetwork):
+	    print("Evaluating network on test sample",network_name)
+	    model.TestModel(1,1,ret_fpr_thresh = 0.3)
+	    print("Evaluating network on external signal sample",network_name)
+	    return
 	if(args.dryRun):
 		exit()
 	#input is TrainModel(epochs=1,oname="",int:verb=1)
 	model.TrainModel(nepochs,batch=100,viz=True,savebest=True,earlystop=early)
 	#needs test data + to make ROC plots
 	print("Evaluating network on test sample",network_name)
-	model.TestModel(1,1,validate_model=True)
-	print("Evaluating network on external signal sample",network_name)
-	model.TestModel_ExternalSignal(sms_data,cols)
+	model.TestModel(1,1,validate_model=True, ret_fpr_thresh = 0.3)
 
 
 def main():
 	parser = argparse.ArgumentParser()
+	parser.add_argument("--parquetpath",help="path to parquet files",required=True)
 	parser.add_argument('--network','-n',help="which set of inputs to run",choices=["iso","shape","isoShape"],required=True)
-	parser.add_argument('--arch','-a',help="which architecture to run",choices=["default","med16","med8","small8"],default="small3")
+	parser.add_argument('--arch','-a',help="which architecture to run",choices=["default","med16","med8","small8","med32","large8"],default="small3")
 	parser.add_argument('--nEpochs',help="number of epochs for training",default=20)
 	parser.add_argument("--dryRun",help="dry run - stats only (don't run network)",action='store_true',default=False)
 	parser.add_argument("--extra",'-e',help='extra string for network name')
 	parser.add_argument("--exclude",help='exclude feature from training',default=None)
 	#parser.add_argument("--reweightClasses",help="reweight classes",default=False,action='store_true')
 	parser.add_argument('--testNetwork',help='evaluate trained network specified by other flags',default=False,action='store_true')
+	parser.add_argument('--debug',help='debug mode',default=False,action='store_true')
 	args = parser.parse_args()
 
 	runDNN(args)
